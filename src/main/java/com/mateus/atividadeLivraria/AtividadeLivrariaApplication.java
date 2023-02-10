485580d68c049270e395e4a1838106faa3302d1c
Mateus Santos <mateus.moura.santos@f1rst.com.br>
2023-02-10 18:44:45 -0300

    Commit inicial :D

---
+++ src/main/java/com/mateus/atividadeLivraria/AtividadeLivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.atividadeLivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeLivrariaApplication.class, args);
+	}
+
+}
